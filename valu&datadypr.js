2c66b6bcf3967be60d8e38fc62cd85ec41ea9b9e
shahrearislam147-spec <shahrearislam147@gmail.com>
2025-10-23 00:45:22 +0600

    Add files via upload

---
+++ valu&datadypr.js
@@ -0,0 +1,8 @@
+// Value & Data Type Example
+let num = 10;           
+let name = "Shahrear";  
+let isStudent = true;   
+
+console.log(num, typeof num);       // 10 number
+console.log(name, typeof name);     // Shahrear string
+console.log(isStudent, typeof isStudent); // true boolean
