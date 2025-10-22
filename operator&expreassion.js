9aa6520ee5effef8fbda47e37e1d927701f0e471
shahrearislam147-spec <shahrearislam147@gmail.com>
2025-10-23 00:49:31 +0600

    Add files via upload

---
+++ operator&expreassion.js
@@ -0,0 +1,12 @@
+// Working with two numbers
+let a = 10;
+let b = 5;
+
+// Expression: sum of two numbers
+let sum = a + b; // '+' is the operator, 'a + b' is the expression
+
+// Expression: product of two numbers
+let product = a * b; // '*' is the operator, 'a * b' is the expression
+
+console.log("Sum:", sum);       // Output: Sum: 15
+console.log("Product:", product); // Output: Product: 50
